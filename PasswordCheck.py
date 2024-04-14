68758d6c75457af2c5928abf3042a2bb2d4a7e89
Jordi Beringues <159556249+jberinguesa@users.noreply.github.com>
2024-04-15 00:11:49 +0200

    Mes

--- PasswordCheck.py
+++ PasswordCheck.py
@@ -3,4 +3,7 @@ def password_security(password):
     # Comprovem si la contrasenya té al menys 8 caràcters
     if len(password) < 8:
         return False
+    # Comprovem si la contrasenya té al menys una lletra majúscula
+    if not any(char.isupper() for char in password):
+        return False
     return True    
